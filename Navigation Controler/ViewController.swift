e3144e932ef7191df393166b38269d6bd7206465
student <student@kitfinstrah.ttknn.net>
2018-02-01 15:16:26 +0300

    Initial Commit

---
+++ Navigation Controler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Navigation Controler
+//
+//  Created by student on 01.02.2018.
+//  Copyright © 2018 Vinogradov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
